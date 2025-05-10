06e4d10c78f68d0de97d1acfb7b320c75a006089
Vlad11ab <breazuvlad11@gmail.com>
2025-05-10 20:26:42 +0300

    no message

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,16 @@
 package app;
 
 
+import app.orderdetail.model.OrderDetail;
+import app.orderdetail.service.OrderDetailCommandService;
+import app.orderdetail.service.OrderDetailCommandServiceSingleton;
+import app.orderdetail.service.OrderDetailQueryService;
+import app.orderdetail.service.OrderDetailQueryServiceSingleton;
+import app.orders.model.Order;
 import app.view.ViewLogin;
 
 public class Main {
     public static void main(String[] args) {
-
-
 
             ViewLogin viewLogin = new ViewLogin();
 
